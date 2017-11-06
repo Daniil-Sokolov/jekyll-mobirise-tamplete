ddbe0f86b5de3f8a32c9f7a0abd1bdba296b4a6f
Daniil-Sokolov <daniil.sok2017@yandex.ru>
2017-11-06 17:04:07 +0300

    preloader

--- assets/common.js
+++ assets/common.js
@@ -17,3 +17,9 @@ $(function(){
       // pjax
       $(document).pjax('a', '#pjax-container', {fragment: '#pjax-container'});
     })
+
+
+
+$(window).load(function() {
+  $('#before-load').find('span').fadeOut().end().delay(400).fadeOut('slow');
+});
